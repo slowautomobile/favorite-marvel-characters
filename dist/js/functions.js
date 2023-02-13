dac483d04515c266c90adc172934df7cb7e04d88
slowautomobile <slowautomobile@protonmail.com>
2023-02-13 12:34:47 +0100

    work in progress

--- dist/js/functions.js
+++ dist/js/functions.js
@@ -11,7 +11,9 @@ function debounceFn(callbackfn, delay) {
 }
 
 function getCharacters(inputEvent) {
-  let searchValue = inputEvent.target.value;
+  searchValue = inputEvent.target.value;
+  charactersGalleryArray = [];
+
   if (searchValue.length) {
     fetch(
       `${marvelApiUrl}?nameStartsWith=${searchValue}&limit=${limitSearch}&apikey=${apiKey}`
@@ -19,16 +21,27 @@ function getCharacters(inputEvent) {
       .then(response => response.json())
       .then(data => {
         let results = data.data.results;
-        if(results > 10) {
-// THIS IS ADDED ! !! 
-        } else {
+        let total = data.data.total;
+        pages = Math.ceil(total / 12);
+        drawGallery(results, pages, 0);
+        marvelAttribution.classList.remove('hide');
 
-          drawGallery(results);
-        }
+        // for(let page = 0; page < pages; page++) {
+        // }
+        // if(pages === 1 ) {
+        // let page = pages;
+        // console.log(pages, 'one page');
+        // } else {
+        // drawGallery(results, pages);
+        // console.log(pages, page);
+        // }
       });
   } else {
-    charactersGallery.innerHTML = '';
-    marvelAttribution.classList.add('hide');
+    charactersGalleryArray = [];
+    mainDiv.innerHTML = '';
+    // charactersGallery.innerHTML = '';
+    tempImagesFragment.innerHTML = '';
+
     displayBookmarkedChars();
   }
 }
@@ -46,21 +59,76 @@ function bookmarkCharacter(character, currentCharacter) {
 }
 
 function displayBookmarkedChars() {
+  bookmarkGallery.innerHTML = '';
   for (let bookmarkedChar of Object.values(localStorage)) {
     drawCharacter(JSON.parse(bookmarkedChar));
+    bookmarkGallery.append(tempImagesFragment);
+  }
+  bookmarkGallery.classList.remove('hide');
+  if (!bookmarkGallery.children.length) {
+    marvelAttribution.classList.remove('hide');
   }
 }
 
-function drawGallery(results) {
-  charactersGallery.innerHTML = '';
-  results.forEach((character, i) => drawCharacter(character, i));
-  marvelAttribution.classList.remove('hide');
+function drawGallery(results, pages, page) {
+  mainDiv.innerHTML = '';
+  // charactersGallery.innerHTML = '';
+  bookmarkGallery.classList.add('hide');
+
+  paginationEl.innerHTML = '';
+  charactersGalleryArray[page] = document.createElement('div');
+  charactersGalleryArray[page].classList.add('charactersGallery');
+  charactersGalleryArray[page].id = `charactersGallery-${page}`;
+
+  // let charactersGallery = document.createElement('div');
+  // charactersGallery.classList.add('charactersGallery');
+  // charactersGallery.id = `charactersGallery-${page}`
+  results.forEach((character, i) => drawCharacter(character, i, page));
+  charactersGalleryArray[page].append(tempImagesFragment);
+
+  // charactersGalleryArray.push(charactersGallery);
+
+  if (pages > 1) {
+    for (let i = 0; i < pages; i++) {
+      let pageEl = document.createElement('p');
+      pageEl.id = `page-${i}`;
+      pageEl.textContent = i + 1;
+      paginationEl.append(pageEl);
+      paginationEl.children[i].addEventListener('click', () => {
+        offset += 12 * i;
+        if (charactersGalleryArray[i].id.slice(18) == i) {
+          mainDiv.append(charactersGalleryArray[i]);
+        } else {
+          // if(charactersGalleryArray[i]) {
+          //   mainDiv.append(charactersGalleryArray[i]);
+          // }
+          mainDiv.innerHTML = '';
+          fetch(
+            `${marvelApiUrl}?nameStartsWith=${searchValue}&offset=${
+              offset * (i + 1)
+            }&limit=${limitSearch}&apikey=${apiKey}`
+          )
+            .then(response => response.json())
+            .then(data => {
+              let results = data.data.results;
+              drawGallery(results);
+            });
+        }
+        charactersGalleryArray[i].classList.remove('hide');
+      });
+      mainDiv.append(charactersGalleryArray[i]);
+      console.log('more find');
+    }
+  } else {
+    console.log('single find');
+  }
+
+  mainDiv.insertAdjacentElement('beforeend', paginationEl);
 }
 
-function drawCharacter(character, i) {
+function drawCharacter(character, i, page) {
   let characterContainer = document.createElement('div');
   characterContainer.id = `character-${i}`;
-  tempImagesFragment.innerHTML = '';
 
   let bookmarkBtn = document.createElement('div');
   bookmarkBtn.id = `bookmarkBtn-${i}`;
@@ -72,10 +140,10 @@ function drawCharacter(character, i) {
 
   characterContainer.innerHTML = `
   ${bookmarkBtn.outerHTML}
-    <div class="image-wrapper">
-    <img src="${character.thumbnail.path}.${character.thumbnail.extension}" class="image">
-    </div>
-    <p class="characterName">${character.name}</p>
+  <div class="image-wrapper">
+  <img src="${character.thumbnail.path}.${character.thumbnail.extension}" class="image">
+  </div>
+  <p class="characterName">${character.name}</p>
   `;
 
   tempImagesFragment.append(characterContainer);
@@ -84,5 +152,6 @@ function drawCharacter(character, i) {
   currentCharacter.addEventListener('click', () => {
     bookmarkCharacter(character, currentCharacter);
   });
-  charactersGallery.append(tempImagesFragment);
+
+  // charactersGallery.append(tempImagesFragment);
 }
--- dist/js/main.js
+++ dist/js/main.js
@@ -1,14 +1,26 @@
 const mainDiv = document.getElementById('main');
-const charactersGallery = document.querySelector('.charactersGallery');
+const bookmarkGallery = document.getElementById('bookmark-gallery');
+// const charactersGallery = document.querySelector('.charactersGallery');
+let charactersGalleryArray = [];
+let pages;
+let page = 0;
+
+
 const marvelAttribution = document.querySelector('.marvel-attribution');
 const searchInput = document.getElementById('search-input-id');
+
+const paginationEl = document.createElement('div');
+paginationEl.classList.add('pagination-wrapper');
 
 
 const apiKey = '19ef48654308361f62403056e6b9f1ca';
 const marvelApiUrl = 'https://gateway.marvel.com:443/v1/public/characters';
-let limitSearch = 20;
+let limitSearch = 12;
+let offset = 0;
+let searchValue;
 
 let tempImagesFragment = new DocumentFragment();
+let tempImagesFragmentBackup = new DocumentFragment();
 
 window.onload = () => {
   displayBookmarkedChars();
